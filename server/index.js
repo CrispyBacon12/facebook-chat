47d953b160b96120ab0926b498dadad9a149b28f
Chris Purssell <chris.purssell@gmail.com>
2017-02-15 18:11:05 +1000

    Add server shell

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+
+var http = require('http').Server(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
