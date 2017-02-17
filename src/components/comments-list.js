de8ad779b8e11e84ea9abbaccdf049ef9489bea1
Chris Purssell <chris.purssell@gmail.com>
2017-02-17 11:03:42 +1000

    Refactor comments-list-item into separate component.

--- src/components/comments-list.js
+++ src/components/comments-list.js
@@ -2,24 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-class CommentsList extends Component { 
+import CommentsListItem from './comments-list-item';
+
+class CommentsList extends Component {
   render() {
     return (
       <ul className="mt-4 mb-4 list-group">
-        { this.props.comments.map(this.renderComment) }
+        { this.props.comments.map(comment => <CommentsListItem comment={comment} />) }
       </ul>
-    );
-  }
-
-  renderComment(comment) {
-    return (
-      <li className="list-group-item flex-column align-items-start" key={comment.id}>
-        <div className="d-flex w-100 justify-content-between">
-          <p className="mb-1 text-primary">{comment.from.name}</p>
-          <small>{comment.created_time}</small>
-        </div>
-        <p className="mb-1">{comment.message}</p>
-      </li>
     );
   }
 }
